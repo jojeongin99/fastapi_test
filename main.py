e1d693e85ae8f5bfd7b54305c4179db1cf23ec67
jojeongin99 <whwjddls99@naver.com>
2023-10-12 15:20:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,14 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+@app.get("/hello")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/hello/jeongin")
+def read_root():
+    return {"Hello": "어서오세요"}
+
+
 app.mount("/", StaticFiles(directory="public", html = True), name="static")
